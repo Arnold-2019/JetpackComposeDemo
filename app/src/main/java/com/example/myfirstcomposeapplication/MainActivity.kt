14ffde6b62ca9d96938565a75ff68008458eaad9
REA-YingWang <rea-yingwang@Yings-MacBook-Pro.local>
2021-05-28 14:34:28 +0800

    feat: TextInput built.

--- app/src/main/java/com/example/myfirstcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstcomposeapplication/MainActivity.kt
@@ -5,33 +5,31 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myfirstcomposeapplication.ui.theme.MyFirstComposeApplicationTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp {
-                MyScreenContent()
-            }
+//            MyApp {
+//                MyScreenContent()
+//            }
+            AnotherScreenContent()
         }
     }
 }
@@ -99,7 +97,68 @@ fun NameList(names: List<String>, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyApp {
-        MyScreenContent()
+//    MyApp {
+//        MyScreenContent()
+//    }
+    AnotherScreenContent()
+}
+
+@Composable
+fun AnotherScreenContent() {
+    Box(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CustomTextField("User Name", "Input your name")
+            Divider(color = Color.Transparent, thickness = 16.dp)
+            CustomTextField("Password", "Input your password")
+        }
     }
+}
+
+@Composable
+private fun CustomTextField(label: String, placeHolderString: String) {
+    val textState = remember { mutableStateOf("") }
+    val focusedState = remember { mutableStateOf(false) }
+
+
+    TextField(
+        modifier = Modifier.border(
+            width = 1.dp,
+            color = if (focusedState.value) Color(0xff333F48) else Color(0xffd2d5da),
+            shape = RoundedCornerShape(4.dp)
+        ),
+        value = textState.value,
+        onValueChange = { textState.value = it },
+        label = {
+            Text(
+                text = label
+            )
+        },
+        placeholder = {
+            Text(
+                text = placeHolderString,
+                fontSize = 16.sp,
+            )
+        },
+        textStyle = TextStyle(
+            fontSize = 16.sp
+        ),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            cursorColor = Color(0xff333F48),
+            focusedLabelColor = Color(0xff697684),
+            unfocusedLabelColor = Color(0xff697684),
+            textColor = Color(0xff333F48),
+            placeholderColor = Color(0xff697684),
+            focusedBorderColor = Color.Transparent,
+            unfocusedBorderColor = Color.Transparent,
+            disabledBorderColor = Color.Transparent,
+            errorBorderColor = Color.Transparent
+        )
+    )
 }
