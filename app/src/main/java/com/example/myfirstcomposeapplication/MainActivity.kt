a892d5d2248950616419a957b7093c6fd859d04f
REA-YingWang <rea-yingwang@Yings-MacBook-Pro.local>
2021-05-28 17:09:33 +0800

    refac: remove redundant code.

--- app/src/main/java/com/example/myfirstcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstcomposeapplication/MainActivity.kt
@@ -3,94 +3,29 @@ package com.example.myfirstcomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.myfirstcomposeapplication.ui.theme.MyFirstComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            MyApp {
-//                MyScreenContent()
-//            }
             AnotherScreenContent()
-        }
-    }
-}
-
-@Composable
-fun MyApp(content: @Composable () -> Unit) {
-    MyFirstComposeApplicationTheme {
-        Surface(color = Color.Yellow) {
-            content()
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    var isSelected by remember { mutableStateOf(false) }
-    val backgroundColor by animateColorAsState(
-        targetValue = if (isSelected) Color.Red else Color.Yellow
-    )
-
-    Text(
-        text = "Hello $name!",
-        modifier = Modifier
-            .padding(24.dp)
-            .background(color = backgroundColor)
-            .clickable { isSelected = !isSelected }
-    )
-}
-
-@Composable
-fun Counter(count: Int, updateCount: (newCount: Int) -> Unit) {
-    Button(onClick = { updateCount(count + 1) }) {
-        Text("I've been clicked $count times")
-    }
-}
-
-@Composable
-fun MyScreenContent(names: List<String> = List(1000) { "Alina & Arnold" }) {
-    val count = remember {
-        mutableStateOf(0)
-    }
-
-    Column {
-        NameList(
-            names = names,
-            modifier = Modifier.weight(1f)
-        )
-        Divider(color = Color.Transparent, thickness = 32.dp)
-        Counter(count.value) {
-            count.value = it
-        }
-    }
-}
-
-@Composable
-fun NameList(names: List<String>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(items = names) { name ->
-            Greeting(name = name)
-            Divider(color = Color.Black)
         }
     }
 }
@@ -98,9 +33,6 @@ fun NameList(names: List<String>, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-//    MyApp {
-//        MyScreenContent()
-//    }
     AnotherScreenContent()
 }
 
@@ -118,7 +50,9 @@ fun AnotherScreenContent() {
             CustomTextField("User Name", "Input your name")
             Divider(color = Color.Transparent, thickness = 16.dp)
             CustomTextField("Password", "Input your password")
+
             Divider(color = Color.Transparent, thickness = 32.dp)
+
             CustomOutlinedTextField("User Name", "Input your name")
             Divider(color = Color.Transparent, thickness = 16.dp)
             CustomOutlinedTextField("Password", "Input your password")
@@ -175,6 +109,9 @@ fun CustomOutlinedTextField(label: String, placeHolderString: String) {
         value = textState.value,
         onValueChange = { textState.value = it },
         label = { Text(text = label) },
+        textStyle = TextStyle(
+            fontSize = 16.sp
+        ),
         placeholder = { Text(text = placeHolderString) },
         colors = TextFieldDefaults.outlinedTextFieldColors(
             cursorColor = Color(0xff333F48),
